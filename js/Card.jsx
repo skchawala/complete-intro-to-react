5d8645d3a724262f9952ba60d600c360f816fee3
satish kumar <sk9929418560@gmail.com>
2019-03-11 16:14:43 +0530

    v5

--- js/Card.jsx
+++ js/Card.jsx
@@ -3,11 +3,11 @@ const PropTypes = require('prop-types');
 
 const Card = (props) =>(
     <div className="show-card">
-                    <img src={`public/img/posters/${props.show.poster}`}   className="show-img"/>
-                    <div className="show-text">
-                        <h3 className="show-title">{ props.show.title}</h3> 
-                        <h4 className="show-year">{ props.show.year}</h4> 
-                        <p className="show-description">{ props.show.description}</p>
+                    <img src={`public/img/posters/${props.show.poster}`}   className="show-card-img"/>
+                    <div className="show-card-text">
+                        <h3 className="show-card-title">{ props.show.title}</h3> 
+                        <h4 className="show-card-year">{ props.show.year}</h4> 
+                        <p className="show-card-description">{ props.show.description}</p>
                     </div>
     </div>  
 )
--- js/Search.jsx
+++ js/Search.jsx
@@ -1,17 +1,42 @@
 const React = require('react');
 const data = require('../data.json')
 const Card = require('./Card')
-const Search = () =>(
-      <div className="container">
-      <div className="shows">
-            {
-            data.shows.map((show)=>(
-               <Card show={show} key={show.imdbID} />
-            ))
-            }
-        </div>
-      </div>
-)
+class Search extends React.Component{
+
+
+  constructor(props){
+    super(props) 
+    this.state = {
+        searchTerm: ""
+    }
+    this.handleSearchTermEvent = this.handleSearchTermEvent.bind(this)
+  }
+ 
+  handleSearchTermEvent(e){
+   this.setState({searchTerm : e.target.value})
+  }
+
+  render(){
+        return  (
+            <div className="container">
+                <header className="header">
+                    <h1 className="brand">svideo</h1>
+                    <input   value={this.state.searchTerm} onChange={this.handleSearchTermEvent} className="search-input" placeholder="search"/>
+                </header>
+                <div className="shows">
+                    {
+                    data.shows
+                    .filter((show)=>`${show.title} ${show.description}`.toLocaleUpperCase().indexOf(this.state.searchTerm.toLocaleUpperCase())>=0)
+                    .map((show)=>(
+                        <Card show={show} key={show.imdbID} />
+                    ))
+                    }
+                </div>
+            </div>
+      )
+    }
+   
+}
 
 
 module.exports = Search;
